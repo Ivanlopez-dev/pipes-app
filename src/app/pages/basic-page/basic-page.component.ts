1572409ff2b2fd6eac2f1d311f2a2d1928aec74b
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-17 10:47:48 +0200

    add:  ⚡ uppercase, lowercase and titlecase cases

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+
+  nameLower = signal('van')
+  nameUpper = signal('VAN')
+  fullName = signal('vaN lóPeZ roDrÍguEZ')
+}
