689e3bdcae62fdceaf6a5701b59448ad986b33c5
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-17 11:51:44 +0200

    add: ⚡ date pipe usecases

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,15 +1,29 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class BasicPageComponent {
 
-  nameLower = signal('van')
-  nameUpper = signal('VAN')
-  fullName = signal('vaN lóPeZ roDrÍguEZ')
+  nameLower = signal('van');
+  nameUpper = signal('VAN');
+  fullName = signal('vaN lóPeZ roDrÍguEZ');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    // Function onCleanup called when "ticking" component's destroyed
+    onCleanup(() => {
+      clearInterval(interval)
+    })
+  });
 }
