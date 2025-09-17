9ba791708356c12f6ec819af185ef997e29696c0
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-17 19:19:42 +0200

    feat: ✨ hero-creator & hero-text-color custom pipes

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
 
 import { heroes } from '../../data/heroes.data';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
