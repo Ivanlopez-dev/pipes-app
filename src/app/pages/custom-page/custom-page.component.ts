1a561ee8c3675796876e134cdc3529b335d8a1d8
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-17 18:57:10 +0200

    feat: ✨ can-fly & hero-color custom pipes

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,13 +1,18 @@
 import { Component, signal } from '@angular/core';
+
+import { heroes } from '../../data/heroes.data';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
 
   name = signal('Iván López');
   upperCase = signal(true);
+  heroes = signal(heroes)
 }
