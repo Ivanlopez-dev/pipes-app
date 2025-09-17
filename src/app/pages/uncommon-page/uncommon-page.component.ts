6d4702d6d10ca113e8b605cfc17657c34bfec1e7
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-17 13:34:31 +0200

    add: ⚡ i18nSelect pipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { I18nSelectPipe } from '@angular/common';
+
 import { CardComponent } from "../../components/card/card.component";
+
+const client1 = {
+  name: 'Iván',
+  gender: 'male',
+  age: 30,
+  address: 'Lugo, Galicia',
+};
+
+const client2 = {
+  name: 'Marta',
+  gender: 'female',
+  age: 37,
+  address: 'Under Ground, Wonderland'
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  // i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2)
+      return;
+    }
+
+    this.client.set(client1);
+  }
+}
