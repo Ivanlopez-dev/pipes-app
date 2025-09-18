d8b09d34ab82e740ae3f967fec9d433ff4ff798f
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-18 10:41:03 +0200

    feat: ✨ hero-filter, last custom pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -9,16 +9,18 @@ import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe, HeroFilterPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
 
   name = signal('Iván López');
   upperCase = signal(true);
-  heroes = signal(heroes)
-  sortBy = signal<keyof Hero | null>(null)
+  heroes = signal(heroes);
+  sortBy = signal<keyof Hero | null>(null);
+  searchQuery = signal('');
 }
